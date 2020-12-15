5546ada8c14a6c13284282ecb44ff3f7aa90183e
Agne <Agne@Agnes-MacBook-Pro.local>
2020-12-15 15:08:39 +0000

    printing strings in python

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+""" OR """
+print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
+print("Charlie " + "Kaylor")
